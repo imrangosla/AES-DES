12fc482f6c1ba7c9140ac3f85ed257ce2b0c8855
milesjmccloskey <milesjmccloskey@gmail.com>
2018-04-11 22:18:10 -0700

    fixing the setup key for AES and DES

--- AES.py
+++ AES.py
@@ -1,12 +1,38 @@
 from Crypto.Cipher import AES
 import sys
+# AES accepts as 128-bit (i.e. 16 byte) plaintext block,
+# a 128, 192, or 256-bit key, and produces
+# a 128-bit ciphertext block.
 
 class AESinterface:
     @staticmethod
     def setKey(key):
-        if (all(ord(c) < 128 for c in key)) and (len(key) == 16):
+        #each hex character is 4 bits so 4 * 32 = 128 bit key
+        #32 characters representing a 128-bit hexadecimal number
+        #ord(c)-returns the ASCII value of c
+        #all() returns true if all elements of the iterable are true
+        #or if iterable is empty
+        #the range in ascii of a-f is 97-102
+        #the range in ascii of 0-9 is 48 through 57
+
+        #0-9, a-f string key
+        asciiRange = (range(48,58) + range(97,103))
+        if (all(ord(c) in asciiRange for c in key)) and (len(key) == 32):
+        #if (all(ord(c) < 128 for c in key)) and (len(key) == 32):
+            # global aes
+            #  aes = AES.new(key, AES.MODE_ECB)
+            # key = key.decode("hex")
+            hexKey=[key[x:x+2] for x in range(0,len(key),2)]
+
+            for i in range(len(hexKey)):
+                hexKey[i] = '\\x'+hexKey[i]
+            print hexKey
+            newKey =''
+            for i in range(len(hexKey)):
+                newKey += hexKey[i]
+            print('newKey: ', newKey)
             global aes
-            aes = AES.new(key, AES.MODE_ECB)
+            aes = AES.new(newKey, AES.MODE_ECB)
         else:
             print "Wrong Key"
             sys.exit()
--- DES.py
+++ DES.py
@@ -3,6 +3,7 @@ import sys
 
 class DESinterface:
     @staticmethod
+    #NEED TO FIX THIS
     def setKey(key):
         if (all(ord(c) < 128 for c in key)) and (len(key) == 16):
             key = key.decode("hex")
@@ -17,10 +18,16 @@ class DESinterface:
     def encrypt(plainText):
         cipherText = ""
         plainText = plainText.rstrip()
+        #takes the plaintext and forevery 8 bits creates an element in the blocks array
         blocks=[plainText[x:x+8] for x in range(0,len(plainText),8)]
         for elements in range(len(blocks)):
             while (len(blocks[elements]) % 8) != 0:
+                #pad the block every 8 bits with an * for the remainder of characters not divisible
+                #by 8
                 blocks[elements] += '*'
+            #we instantianted the des object in the setKey function call, and
+            #here we are encrypting it from the blocks of 8 bit chunks of the plaintext
+            #and appending to the string ciphertext
             cipherText += des.encrypt(blocks[elements])
         return cipherText
 
